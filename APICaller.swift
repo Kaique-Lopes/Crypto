ff27562ec0d7358bce00339d563f3f2c8d99ed94
Kaique <kaique.lopes3271@gmail.com>
2021-09-11 19:24:58 -0300

    Update APICaller.swift

--- APICaller.swift
+++ APICaller.swift
@@ -12,7 +12,11 @@ final class APICaller {
     static let shared = APICaller()
     
     private struct Constants {
-        static let apiKey = ""
+        static let apiKey = "B6955A7B-A2EC-427B-B27F-D91F535C504A"
+        
+        // List All exchanges https://rest-sandbox.coinapi.io/v1/exchanges/?apikey=B6955A7B-A2EC-427B-B27F-D91F535C504A
+        
+        static let assetsEndpoint = "https://rest-sandbox.coinapi.io/v1/assets/"
     }
     
     private init() {}
@@ -22,6 +26,22 @@ final class APICaller {
     public func getAllCryptoData(
         completion: @escaping (Result<[String], Error>) -> Void
     ) {
+        guard let url = URL(string: Constants.assetsEndpoint + "?apikey=" + Constants.apiKey) else {
+            return
+        }
         
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            do {
+                //Decode Response
+            }
+            catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
     }
 }
