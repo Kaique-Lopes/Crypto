ce93660d98516ff3587ae78b58e38bd3f18fc8ec
Kaique <kaique.lopes3271@gmail.com>
2021-09-11 18:43:57 -0300

    APICaller

---
+++ APICaller.swift
@@ -0,0 +1,13 @@
+//
+//  APICaller.swift
+//  Crypto
+//
+//  Created by Kaique Lopes de Oliveira on 11/09/21.
+//
+
+import Foundation
+
+
+final class APICaller {
+    static let shared = APICaller()
+}
